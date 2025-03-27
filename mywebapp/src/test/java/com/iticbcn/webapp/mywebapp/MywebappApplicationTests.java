2e6b52a16192bbcb986c40e8dfd06aebb63034c2
karen <karenquispego97@gmail.com>
2025-03-27 20:52:49 +0100

    commit clase 27/3

---
+++ mywebapp/src/test/java/com/iticbcn/webapp/mywebapp/MywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iticbcn.webapp.mywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
